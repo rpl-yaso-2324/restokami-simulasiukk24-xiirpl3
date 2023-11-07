97057001559038dfb7f2b6f4ec2e8e80f1a0a0e3
Harisandrian14 <harisandrian148@gmail.com>
2023-11-07 14:10:29 +0700

    nambahin js

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ function login() {
     password.value = ""
     
 }
+
+const menu = [
+    {
+        name : "ayam goleng",
+        imgURL : "./assets/img/store.png",
+        desc : "ayam goleng + nasi enak banget",
+        price : 15000,
+    },
+    {
+        name : "ayam goleng",
+        imgURL : "./assets/img/store.png",
+        desc : "ayam goleng + nasi enak banget",
+        price : 15000,
+    },
+    {
+        name : "ayam goleng",
+        imgURL : "./assets/img/store.png",
+        desc : "ayam goleng + nasi enak banget",
+        price : 15000,
+    },
+    
+]
+
+const makanan = document.getElementById('makanan')
+
+menu.map(item => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.innerHTML = `
+    <div class="card1">
+    <img src=${item.imgURL} alt="" class="gmbr-menu">
+    <section>
+        <p>${item.name}</p>
+        <p>${item.desc}</p>
+    </section>
+</div>
+    `;
+    makanan.appendChild(cardElement);
+});

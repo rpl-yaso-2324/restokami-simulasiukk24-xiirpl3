17954d5037a452f830b51044fc088df83fd4f3d6
MuhamadRido25 <ridorahadian25@gmail.com>
2024-01-09 13:23:51 +0700

    menu new

---
+++ js/menu.js
@@ -0,0 +1,104 @@
+localStorage.removeItem("total");
+
+var menu = [
+  {
+    id: 0,
+    name: "Paket 1",
+    desc: " Bakso + Mie + Teh",
+    price: 36000,
+    count: 0,
+  },
+  {
+    id: 1,
+    name: "Paket 2",
+    desc: "Bakso Beranak + Teh",
+    price: 36000,
+    count: 0,
+  },
+  {
+    id: 2,
+    name: "Paket 3",
+    desc: "Mie Ayam + Teh",
+    price: 36000,
+    count: 0,
+  },
+  {
+    id: 3,
+    name: "Paket 4",
+    desc: "Mie ayam + Bakso <br> + Mie + Teh",
+    price: 40000,
+    count: 0,
+  },
+  {
+    id: 4,
+    name: "Paket 5",
+    desc: "Mie ayam + Bakso <br> + Bakso Beranak + Teh",
+    price: 40000,
+    count: 0,
+  }
+];
+
+// add menu card
+const menuSection = document.querySelector(".menu-section");
+
+let card = ``;
+
+menu.map((data) => {
+  card += innerCardMenu(data.name, data.desc, data.price, data.count, data.id);
+  menuSection.innerHTML = card;
+});
+
+function innerCardMenu(name, desc, price, count, id) {
+  return `<!-- card -->
+      <div class="card">
+          <button class="kategori" aria-readonly="true">Paket</button>
+          <hr class="line" />
+          <div class="container-content">
+              <div>
+                  <img
+                      src="./images/example-product/indomie.jpg"
+                      alt=""
+                      class="produk-img"
+                  />
+              </div>
+              <div class="content">
+                  <p>${name}</p>
+                  <p>${desc}</p>
+                  <div class="content2">
+                  <p>${price}</p>
+                  <button class="kurang" id="pesan${id}">Pesan</button>
+                </div>
+              </div>
+          </div>
+      </div>
+      `;
+}
+
+// add count
+let order = [];
+
+menu.map((data) => {
+  const pesan = document.getElementById(`pesan${data.id}`);
+
+  pesan.addEventListener("click", (e) => {
+    e.preventDefault();
+    const jumlah = prompt("Berapa yang anda pesan?");
+    // console.log("ada jumlah? " + jumlah);
+
+    // JIka pengguna menekan pada prompt "OK"
+    // Akan masuk ke kondisi dibawah
+    if (jumlah !== null) {
+      // Jika pengguna mengisi prompt atau prompt tidak kosong akan masuk ke kondisi dibawah
+      if (jumlah !== "") {
+        const total = data.price * jumlah; // hitung total hanya untuk item saat ini
+        // console.log(`total for ${data.name}: ${total}`); // Cek total
+        localStorage.setItem("total", total);
+        window.location.href = "order.html";
+      } else {
+        // Pengguna menekan "OK" tanpa memasukkan nilai
+        alert("Anda harus memasukkan jumlah pesanan.");
+        // Tambahkan logika lain jika diperlukan
+      }
+    }
+  });
+});
